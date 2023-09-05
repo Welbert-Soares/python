8a3d77514381399cde5c34192a055e5bfdca581e
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2023-09-05 19:01:46 -0300

    Create aula07a.py

---
+++ aprendendo_python/aula07a.py
@@ -0,0 +1,15 @@
+# Operadores Aritmétricos
+# +(adição)  -(subtração) *(multiplicação) /(divisão) **(potencialização)  //(divisão inteira) %(resto da divisão) ==(igual)
+# Ordem de Precedência
+#        1° () ,           2 ° * * ,           3° * , / , // , % .          4° + , - .
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma é {}, o produto é {} e a divisão é {:,3}.'.format(s, m , d))
+print('Divisão inteira {} e potência {}.'.format(di, e))
