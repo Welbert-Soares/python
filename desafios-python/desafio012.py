9f2c67709f42ad2ec3d10631895db1b3b39165cc
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2023-09-07 02:14:15 -0300

    desafio012

--- desafios-python/desafio012.py
+++ desafios-python/desafio012.py
@@ -1,4 +1,7 @@
 print('====== DESAFIO 012 ======')
 # programa que lê o preço de um produto e calcula 5% de juros sobre o mesmo.
 
-price = 
+price = float(input('Qual o preço do produto? '))
+juros = price * 5 / 100
+new_valor = price + juros
+print('O juros de cinco por cento sobre o valor R${:.2f}, será R${:.2f}, ao total R${:.2f}.'.format(price, juros, new_valor))
