6ddd0ed074ebad21722316fc18fcf6283a438c39
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2023-09-07 14:53:29 -0300

    desafio013

--- desafios-python/desafio013.py
+++ desafios-python/desafio013.py
@@ -2,4 +2,6 @@ print('====== DESAFIO 013 ======')
 # programa que leia o salário de um funcionário e retorne com 15% de aumento
 
 salario = float(input('Qual o valor do salário? R$'))
-print(' ')
+new_money = salario + (salario * 15 / 100)
+print('O salário é R${:.2f}, com o aumento de 15% ficará R${:.2f}'.format(salario, new_money))
+print('-'*25)
